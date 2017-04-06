3447aed8714ec3c086eb80fa5324ceeebb4c4bd5
NickLatkovich <nicklatkovich@gmail.com>
2017-04-06 11:01:25 +0300

    Init

---
+++ CGaG_Lab04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CGaG_Lab04 {
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main( ) {
+            using (MainThread mainThread = new MainThread( ))
+                mainThread.Run( );
+        }
+    }
+#endif
+}
